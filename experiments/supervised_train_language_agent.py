747fd91ac0bb75d958ca75f8d72225e0e9fc95f6
moghis <moghisfereidooni@yahoo.com>
2023-08-16 17:14:35 -0400

    updated per_device_eval_batch_size args

--- experiments/supervised_train_language_agent.py
+++ experiments/supervised_train_language_agent.py
@@ -393,7 +393,7 @@ def parse_args():
     parser.add_argument(
         "--per_device_eval_batch_size",
         type=int,
-        default=4,
+        default=8,
         help="Batch size (per device) for the evaluation dataloader.",
     )
     parser.add_argument(
