d9065992d1a7f3b4da689ecd2a876e029c922fcf
moghis <moghisfereidooni@yahoo.com>
2023-12-17 13:16:22 -0500

    added the config for ppo only

--- experiments/bash_files/test_only_ppo/slurm_test_run.sh
+++ experiments/bash_files/test_only_ppo/slurm_test_run.sh
@@ -14,4 +14,7 @@
 python /u/spa-d2/grad/mfe261/Projects/Grounding_LLMs_with_online_RL/lamorel/lamorel/src/lamorel_launcher/launch.py \
         --config-path=/u/spa-d2/grad/mfe261/Projects/Grounding_LLMs_with_online_RL/experiments/configs \
         --config-name=my_local_test_config \
-        rl_script_args.path=/u/spa-d2/grad/mfe261/Projects/Grounding_LLMs_with_online_RL/experiments/test_webshop.py
+        rl_script_args.path=/u/spa-d2/grad/mfe261/Projects/Grounding_LLMs_with_online_RL/experiments/test_webshop.py \
+        rl_script_args.id_expe=flan_t5_large_2_observations_only_ppo_1000000_steps \
+        rl_script_args.saving_path_model=$SCRATCH/storage/models \
+        rl_script_args.saving_path_logs=$SCRATCH/storage/logs
