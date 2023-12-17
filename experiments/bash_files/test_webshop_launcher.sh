250763043838855621a9351a97e3315b15eba762
moghis <moghisfereidooni@yahoo.com>
2023-12-17 14:20:20 -0500

    deleted a duplicate file

--- experiments/bash_files/test_webshop_launcher.sh
+++
@@ -1,4 +0,0 @@
-python /u/spa-d4/grad/mfe261/Projects/Grounding_LLMs_with_online_RL/lamorel/lamorel/src/lamorel_launcher/launch.py \
-        --config-path=/u/spa-d4/grad/mfe261/Projects/Grounding_LLMs_with_online_RL/experiments/configs \
-        --config-name=my_local_test_config \
-        rl_script_args.path=/u/spa-d4/grad/mfe261/Projects/Grounding_LLMs_with_online_RL/experiments/test_webshop.py
