8dc12dd66650826d22b72940ce70e52792e4914f
moghis <moghisfereidooni@yahoo.com>
2023-12-17 14:56:02 -0500

    adjucted the config files

--- experiments/bash_files/train_ppo/slurm_train_ppo_run.sh
+++ experiments/bash_files/train_ppo/slurm_train_ppo_run.sh
@@ -13,7 +13,11 @@
 
 
 
-python /home/mfe261/PPO_webshop/lamorel/lamorel/src/lamorel_launcher/launch.py \
-        --config-path=/home/mfe261/PPO_webshop/experiments/configs \
+python $PROJECT/lamorel/lamorel/src/lamorel_launcher/launch.py \
+        --config-path=$PROJECT/experiments/configs \
         --config-name=my_local_config \
-        rl_script_args.path=/home/mfe261/PPO_webshop/experiments/train_language_agent.py
+        rl_script_args.path=$PROJECT/experiments/train_language_agent.py \
+        rl_script_args.saving_path_model=$SCRATCH/storage/models \
+        rl_script_args.saving_path_logs=$SCRATCH/storage/logs \
+        rl_script_args.run_name=flan_t5_large_only_ppo \
+        rl_script_args.environment_args.extra_search_path=$SCRATCH/data/goal_query_predict.json
