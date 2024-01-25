df9c919db9c2ef81e6a2ed11ae3c686480388ca6
moghis <moghisfereidooni@yahoo.com>
2024-01-25 15:39:09 -0500

    some modifications

--- experiments/bash_files/lcc_drrn_test_run.sh
+++ experiments/bash_files/lcc_drrn_test_run.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=Flan_T5_webshop # Job name
 #SBATCH -e Flan_T5_webshop_slurm-%j.err # Error file for this job.
 #SBATCH -o Flan_T5_webshop_slurm-%j.out # Output file for this job.
-#SBATCH --partition=P4V16_HAS16M128_L # Partition/queue to run the job in. (REQUIRED)
-#SBATCH -A gol_msi290_uksr # Project allocation account name (REQUIRED)
+#SBATCH --partition=??? # Partition/queue to run the job in. (REQUIRED)
+#SBATCH -A ??? # Project allocation account name (REQUIRED)
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=32
 #SBATCH --hint=nomultithread
